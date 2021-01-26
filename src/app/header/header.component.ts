56f05e8cb0a117da57e2b12fe519ee05c20100ec
moulspidi <hatim-rian@hotmail.fr>
2021-01-26 23:19:10 +0100

    first1 commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  currentSection = 'home';
+  navbarOpen = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+  windowScroll() {
+    const navbar = document.getElementById('navbar');
+    if (document.body.scrollTop >= 50 || document.documentElement.scrollTop > 50) {
+      navbar.classList.add('nav-sticky');
+    } else {
+      navbar.classList.remove('nav-sticky');
+    }
+  }
+
+  /**
+   * Section changed method
+   * @param sectionId specify the current sectionID
+   */
+  onSectionChange(sectionId: string) {
+    this.currentSection = sectionId;
+  }
+
+  /**
+   * Toggle navbar
+   */
+  toggleMenu() {
+    document.getElementById('navbarCollapse').classList.toggle('show');
+  }
+
+}
